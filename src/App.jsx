6f219c1c4aac7f6c70f792990f45f7b0f63f17cb
omrajsharma <omraj7413@gmail.com>
2024-02-18 11:58:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      app
+      <Header />
+    </div>
+  )
+}
+
+export default App
